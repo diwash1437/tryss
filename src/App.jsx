82f03233743bc1bd2ecb599d775028f530fb439e
diwash1437 <diwashlimbu47@gmail.com>
2026-01-14 02:28:00 +0900

    add deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Restaurant from "./Recipe/Restaurant";
-import { PickurusuRecipe } from './Recipe/PickurusuRecipe/PickurusuRecipe';
-import { AlcholOrder } from './page/AlcholOrder';
-import { VegetableSoup } from './Recipe/VegetableSoup';
-import { VegetableSoupOrder } from './order/VegetableSoupOrder';
-import { Lunch } from './premium/Lunch';
-import NabataColtd153 from './order/NabataColtd153';
+
 
 function App() {
   return (
